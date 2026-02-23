388e2c1b81d70d44021270c04da37a1662ef1f2f
Milite <jh607035@gmail.com>
2026-02-23 21:01:49 +0900

    장대한 염원/암흑의 횃불 데이터 입력

--- js/data/weapons/sword/grandVision.js
+++ js/data/weapons/sword/grandVision.js
@@ -14,51 +14,52 @@ const GrandVisionData = {
     "option3": {
         "name": "고통 · 간절한 소망",
         "keyword": "고통",
-        "stackRule": ,
-        "maxStacks": ,
+        "stackRule": "unique",
+        "maxStacks": 1,
         "minLevel": 1,
         "maxLevel": 9,
         "maxBreakthrough": 6,
-        "description": ,
+        "description": "장착자 오리지늄 아츠 강도 증가, 조건에 따라 배틀 스킬 혹은 궁극기가 주는 물리 피해 증가",
 
         "personalEffect": {
-            "stat": ,
-            "description": ,
+            "stat": "artsEnhance",
+            "description": "장착자 오리지늄 아츠 강도 증가",
             "values": {
-                "1": ,
-                "2": ,
-                "3": ,
-                "4": ,
-                "5": ,
-                "6": ,
-                "7": ,
-                "8": ,
-                "9": 
+                "1": 30,
+                "2": 36,
+                "3": 42,
+                "4": 48,
+                "5": 54,
+                "6": 60,
+                "7": 66,
+                "8": 72,
+                "9": 84
             }
         },
 
         "keywordEffect": {
-            "stat": ,
-            "target": ,
-            "description": ,
+            "stat": "physicalDamageIncrease",
+            "target": "self",
+            "description": "배틀 스킬 및 궁극기가 주는 물리 피해 증가",
             "conditions": {
-                "userToggleable": true
+                "userToggleable": true,
+                "skills": ["battleSkill", "ultimate"]
             },
             "values": {
-                "1": ,
-                "2": ,
-                "3": ,
-                "4": ,
-                "5": ,
-                "6": ,
-                "7": ,
-                "8": ,
-                "9": 
+                "1": 36,
+                "2": 43.2,
+                "3": 50.4,
+                "4": 57.6,
+                "5": 64.8,
+                "6": 72,
+                "7": 79.2,
+                "8": 86.4,
+                "9": 100.8
             }
         }
     }
 }
 
 if (typeof window !== 'undefined') {
-    window. = ;
+    window.GrandVisionData = GrandVisionData;
 }
--- js/data/weapons/sword/umbralTorch.js
+++ js/data/weapons/sword/umbralTorch.js
@@ -1,64 +1,85 @@
 //DamageCalc\js\data\weapons\sword\umbralTorch.js
 
-const  = {
-    "id": ,
-    "name": ,
+const UmbralTorchData = {
+    "id": "umbral_torch",
+    "name": "암흑의 횃불",
     "type": "sword",
     "rarity": 6,
 
-    "weaponAtk": ,
+    "weaponAtk": 490,
 
-    "option1": ,
-    "option2": ,
+    "option1": "intellect3",
+    "option2": "heatDamageIncrease3",
 
     "option3": {
-        "name": ,
-        "keyword": ,
-        "stackRule": ,
-        "maxStacks": ,
+        "name": "고통 · 쌓이는 탐욕",
+        "keyword": "고통",
+        "stackRule": "stack",
+        "maxStacks": 2,
         "minLevel": 1,
         "maxLevel": 9,
         "maxBreakthrough": 6,
-        "description": ,
+        "description": "장착자 공격력 증가, 조건에 따라 열기와 자연 피해 증가(최대 2스택)",
 
         "personalEffect": {
-            "stat": ,
-            "description": ,
+            "stat": "atkIncrease",
+            "description": "장착자 공격력 증가",
             "values": {
-                "1": ,
-                "2": ,
-                "3": ,
-                "4": ,
-                "5": ,
-                "6": ,
-                "7": ,
-                "8": ,
-                "9": 
+                "1": 7,
+                "2": 8.4,
+                "3": 9.8,
+                "4": 11.2,
+                "5": 12.6,
+                "6": 14,
+                "7": 15.4,
+                "8": 16.8,
+                "9": 19.6
             }
         },
 
-        "keywordEffect": {
-            "stat": ,
-            "target": ,
-            "description": ,
-            "conditions": {
-                "userToggleable": true
+        "keywordEffect": [
+            {
+                "stat": "heatDamageIncrease",
+                "target": "self",
+                "description": "열기 피해 증가 적용",
+                "conditions": {
+                    "userToggleable": true
+                },
+                "values": {
+                    "1": 8,
+                    "2": 9.6,
+                    "3": 11.2,
+                    "4": 12.8,
+                    "5": 14.4,
+                    "6": 16,
+                    "7": 17.6,
+                    "8": 19.2,
+                    "9": 22.4
+                }
             },
-            "values": {
-                "1": ,
-                "2": ,
-                "3": ,
-                "4": ,
-                "5": ,
-                "6": ,
-                "7": ,
-                "8": ,
-                "9": 
+            {
+                "stat": "natureDamageIncrease",
+                "target": "self",
+                "description": "자연 피해 증가 적용",
+                "conditions": {
+                    "userToggleable": true
+                },
+                "values": {
+                    "1": 8,
+                    "2": 9.6,
+                    "3": 11.2,
+                    "4": 12.8,
+                    "5": 14.4,
+                    "6": 16,
+                    "7": 17.6,
+                    "8": 19.2,
+                    "9": 22.4
+                }
             }
-        }
+        ]
     }
 }
 
 if (typeof window !== 'undefined') {
-    window. = ;
+    window.UmbralTorchData = UmbralTorchData;
 }
