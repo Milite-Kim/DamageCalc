39b6ad655e9753426b4066fca98d538adfc64f4a
Milite <jh607035@gmail.com>
2026-02-25 21:22:26 +0900

    강철의 여운/항로의 개척자 데이터 입력

--- js/data/weapons/handcannon/navigator.js
+++ js/data/weapons/handcannon/navigator.js
@@ -1,64 +1,161 @@
 //DamageCalc\js\data\weapons\handcannon\navigator.js
 
-const  = {
-    "id": ,
-    "name": ,
+const NavigatorData = {
+    "id": "navigator",
+    "name": "항로의 개척자",
     "type": "handcannon",
     "rarity": 6,
 
-    "weaponAtk": ,
+    "weaponAtk": 490,
 
-    "option1": ,
-    "option2": ,
+    "option1": "intellect3",
+    "option2": "cryoDamageIncrease3",
 
     "option3": {
-        "name": ,
-        "keyword": ,
-        "stackRule": ,
-        "maxStacks": ,
+        "name": "고통 · 외로운 항해",
+        "keyword": "고통",
+        "stackRule": "unique",
+        "maxStacks": 1,
         "minLevel": 1,
         "maxLevel": 9,
         "maxBreakthrough": 6,
-        "description": ,
+        "description": "장착자의 치명타 확률 증가. 조건에 따라 냉기와 자연 피해 증가 및 치명타 확률 증가/추가 조건에 따라 효과가 2배로 증가",
 
         "personalEffect": {
-            "stat": ,
-            "description": ,
+            "stat": "critRate",
+            "description": "치명타 확률 증가",
             "values": {
-                "1": ,
-                "2": ,
-                "3": ,
-                "4": ,
-                "5": ,
-                "6": ,
-                "7": ,
-                "8": ,
-                "9": 
+                "1": 3.5,
+                "2": 4.2,
+                "3": 4.9,
+                "4": 5.6,
+                "5": 6.3,
+                "6": 7,
+                "7": 7.7,
+                "8": 8.4,
+                "9": 9.8
             }
         },
 
-        "keywordEffect": {
-            "stat": ,
-            "target": ,
-            "description": ,
-            "conditions": {
-                "userToggleable": true
+        "keywordEffect": [
+            {
+                "stat": "cryoDamageIncrease",
+                "target": "self",
+                "description": "냉기 피해 증가",
+                "conditions": {
+                    "userToggleable": true
+                },
+                "values": {
+                    "1": 3.5,
+                    "2": 4.2,
+                    "3": 4.9,
+                    "4": 5.6,
+                    "5": 6.3,
+                    "6": 7,
+                    "7": 7.7,
+                    "8": 8.4,
+                    "9": 9.8
+                }
             },
-            "values": {
-                "1": ,
-                "2": ,
-                "3": ,
-                "4": ,
-                "5": ,
-                "6": ,
-                "7": ,
-                "8": ,
-                "9": 
+            {
+                "stat": "natureDamageIncrease",
+                "target": "self",
+                "description": "자연 피해 증가",
+                "conditions": {
+                    "userToggleable": true
+                },
+                "values": {
+                    "1": 3.5,
+                    "2": 4.2,
+                    "3": 4.9,
+                    "4": 5.6,
+                    "5": 6.3,
+                    "6": 7,
+                    "7": 7.7,
+                    "8": 8.4,
+                    "9": 9.8
+                }
+            },
+            {
+                "stat": "critRate",
+                "target": "self",
+                "description": "치명타 확률 증가",
+                "conditions": {
+                    "userToggleable": true
+                },
+                "values": {
+                    "1": 2,
+                    "2": 2.4,
+                    "3": 2.8,
+                    "4": 3.2,
+                    "5": 3.6,
+                    "6": 4,
+                    "7": 4.4,
+                    "8": 4.8,
+                    "9": 5.6
+                }
+            },
+            {
+                "stat": "cryoDamageIncrease",
+                "target": "self",
+                "description": "강화 냉기 피해 증가",
+                "conditions": {
+                    "userToggleable": true
+                },
+                "values": {
+                    "1": 7,
+                    "2": 8.4,
+                    "3": 9.8,
+                    "4": 11.2,
+                    "5": 12.6,
+                    "6": 14,
+                    "7": 15.4,
+                    "8": 16.8,
+                    "9": 19.6
+                }
+            },
+            {
+                "stat": "natureDamageIncrease",
+                "target": "self",
+                "description": "강화 자연 피해 증가",
+                "conditions": {
+                    "userToggleable": true
+                },
+                "values": {
+                    "1": 7,
+                    "2": 8.4,
+                    "3": 9.8,
+                    "4": 11.2,
+                    "5": 12.6,
+                    "6": 14,
+                    "7": 15.4,
+                    "8": 16.8,
+                    "9": 19.6
+                }
+            },
+            {
+                "stat": "critRate",
+                "target": "self",
+                "description": "강화 치명타 확률 증가",
+                "conditions": {
+                    "userToggleable": true
+                },
+                "values": {
+                    "1": 4,
+                    "2": 4.8,
+                    "3": 5.6,
+                    "4": 6.4,
+                    "5": 7.2,
+                    "6": 8,
+                    "7": 8.8,
+                    "8": 9.6,
+                    "9": 11.2
+                }
             }
-        }
+        ]
     }
 }
 
 if (typeof window !== 'undefined') {
-    window. = ;
+    window.NavigatorData = NavigatorData;
 }
--- js/data/weapons/sword/sunderingSteel.js
+++ js/data/weapons/sword/sunderingSteel.js
@@ -1,64 +1,64 @@
 //DamageCalc\js\data\weapons\sword\sunderingSteel.js
 
-const  = {
-    "id": ,
-    "name": ,
+const SunderingSteelData = {
+    "id": "sundering_steel",
+    "name": "강철의 여운",
     "type": "sword",
-    "rarity": 6,
+    "rarity": 5,
 
-    "weaponAtk": ,
+    "weaponAtk": 411,
 
-    "option1": ,
-    "option2": ,
+    "option1": "agility2",
+    "option2": "physicalDamageIncrease2",
 
     "option3": {
-        "name": ,
-        "keyword": ,
-        "stackRule": ,
-        "maxStacks": ,
+        "name": "기예 · 옛 불의 잔향",
+        "keyword": "기예",
+        "stackRule": "stack",
+        "maxStacks": 2,
         "minLevel": 1,
         "maxLevel": 9,
         "maxBreakthrough": 6,
-        "description": ,
+        "description": "장착자 공격력 증가, 조건에 따라 장착자의 공격력 증가",
 
         "personalEffect": {
-            "stat": ,
-            "description": ,
+            "stat": "atkIncrease",
+            "description": "공격력 증가",
             "values": {
-                "1": ,
-                "2": ,
-                "3": ,
-                "4": ,
-                "5": ,
-                "6": ,
-                "7": ,
-                "8": ,
-                "9": 
+                "1": 5,
+                "2": 6,
+                "3": 7,
+                "4": 8,
+                "5": 9,
+                "6": 10,
+                "7": 11,
+                "8": 12,
+                "9": 14
             }
         },
 
         "keywordEffect": {
-            "stat": ,
-            "target": ,
-            "description": ,
+            "stat": "atkIncrease",
+            "target": "self",
+            "description": "조건부 공격력 증가",
             "conditions": {
                 "userToggleable": true
             },
             "values": {
-                "1": ,
-                "2": ,
-                "3": ,
-                "4": ,
-                "5": ,
-                "6": ,
-                "7": ,
-                "8": ,
-                "9": 
+                "1": 7.5,
+                "2": 9,
+                "3": 10.5,
+                "4": 12,
+                "5": 13.5,
+                "6": 15,
+                "7": 16.5,
+                "8": 18,
+                "9": 21
             }
         }
     }
 }
 
 if (typeof window !== 'undefined') {
-    window. = ;
+    window.SunderingSteelData = SunderingSteelData;
 }
